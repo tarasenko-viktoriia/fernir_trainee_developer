3fd54e8f814639c12149262554ff106ddd7546a3
victoriataracenko <victoriataracenko0412@gmail.com>
2025-03-24 22:05:42 +0200

    edited footer, header, added burger menu

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 "use strict"
+
+document.addEventListener("click", function(e){
+   const targetElement = e.target
+   if(targetElement.closest(".icon-menu")) {
+      document.documentElement.classList.toggle("open-menu")
+      e.preventDefault()
+   }
+})
