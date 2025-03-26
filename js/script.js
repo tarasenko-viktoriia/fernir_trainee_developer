bac5b639a7b38a15d581bdb7e17047a5994544a4
victoriataracenko <victoriataracenko0412@gmail.com>
2025-03-26 21:46:28 +0200

    edited slider

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 "use strict"
-
+document.addEventListener("DOMContentLoaded", function () {
+   slidersInit();
+});
 document.addEventListener("click", function(e){
    const targetElement = e.target
    if(targetElement.closest(".icon-menu")) {
@@ -7,3 +9,47 @@ document.addEventListener("click", function(e){
       e.preventDefault()
    }
 })
+
+function slidersInit() {
+	const sliderListFiveItems = new Swiper('.items-slider__list', {
+		slidesPerGroup: 2,
+		slidesPerView: 2,
+		//spaceBetween: 30,
+		// loop: true,
+		// Navigation arrows
+		navigation: {
+			prevEl: '.slider-controls__arrow--left',
+			nextEl: '.slider-controls__arrow--right',
+		},
+	
+		// breakpoints: {
+		// 	// when window width is >= 480px
+		// 	320: {
+		// 		slidesPerView: 1.8,
+		// 		spaceBetween: 20
+		// 	},
+		// 	// when window width is >= 600px
+		// 	600: {
+		// 		slidesPerView: 3,
+		// 		spaceBetween: 20
+		// 	},
+		// 	// when window width is >= 800px
+		// 	850: {
+		// 		slidesPerGroup: 1,
+		// 		slidesPerView: 4,
+		// 		spaceBetween: 20
+		// 	},
+		// 	992: {
+		// 		slidesPerGroup: 4,
+		// 		slidesPerView: 4,
+		// 		spaceBetween: 20
+		// 	},
+		// 	// when window width is >= 1200px
+		// 	1200: {
+		// 		slidesPerGroup: 5,
+		// 		slidesPerView: 5,
+		// 		spaceBetween: 30
+		// 	}
+		// }
+	});
+}
